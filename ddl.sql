8ba0df8fc7fe31ad301479518471802412bd29e9
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-10-07 23:08:34 -0300

    Alter table

--- ddl.sql
+++ ddl.sql
@@ -1,6 +1,9 @@
 /* DDL */
 /* Data Definition Language */
 
+
+/* UTILS */
+use cadastro;
 show table status;
 drop database assessorias;
 show databases;
@@ -8,16 +11,13 @@ show tables;
 describe pessoas;
 desc pessoas;
 
-create database cadastro 
-default character set utf8 
-default collate utf8_general_ci; 
 
-use cadastro;
+/* CREATE DATA BASE */
+create database cadastro default character set utf8 default collate utf8_general_ci; 
 
-SELECT * FROM cadastro.pessoas;
-select * from pessoas where sexo = 'F';
-select * from pessoas where sexo = 'F' and peso > 60;
 
+
+/* CREATE TABLE AND COLUNS */
 CREATE TABLE pessoas (
     id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
@@ -29,5 +29,51 @@ CREATE TABLE pessoas (
     PRIMARY KEY (id)
 )  DEFAULT CHARSET = utf8;
 
+create table if not exists cursos(
+	nome varchar(30) not null unique,
+	descricao text,
+    carga int unsigned,
+    totaulas int unsigned,
+    ano year default '2018'
+) default charset = utf8;
+
+
+
+/* Add Columns */
+/* column optional */
+alter table pessoas add column profissao varchar(20);
+alter table pessoas add profissao varchar(20);
+alter table pessoas add column profissao varchar(20) after nome;
+alter table pessoas add column teste varchar(20) first;
+
+
+
+/* Remove Columns */
+alter table pessoas drop column profissao;
+alter table pessoas drop profissao;
+
+
+
+/* Modify Column */
+alter table pessoas modify column profissao varchar(30);
+alter table pessoas modify column profissao varchar(30) not null default '';
+alter table pessoas modify profissao varchar(30);
+
+
+
+/* Rename Colums */
+alter table pessoas change column profissao prof varchar(40);
+
+
+
+/* Rename Table */
+alter table pessoas rename pessoal;
+
+
+
+/* SELECTS */
+SELECT * FROM cadastro.pessoas;
+select * from pessoas where sexo = 'F';
+select * from pessoas where sexo = 'F' and peso > 60;
 
 
