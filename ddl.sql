9d1a78d8de33ef6cc3fe9e3357af22fbc9fc44b7
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-10-07 22:26:16 -0300

    DML

--- ddl.sql
+++ ddl.sql
@@ -1,3 +1,6 @@
+/* DDL */
+/* Data Definition Language */
+
 show table status;
 drop database assessorias;
 show databases;
@@ -26,13 +29,5 @@ CREATE TABLE pessoas (
     PRIMARY KEY (id)
 )  DEFAULT CHARSET = utf8;
 
-insert into pessoas 
-(nome, idade, sexo, peso, altura, nacionalidade) 
-values 
-('Chico', '1970/02/07', 'M', 55.5, 1.72, 'EUA'),
-('Maria', '2000/01/05', 'M', 34.5, 1.72, 'Brasileiro');
 
-insert into pessoas values 
-(default, 'Ludiana', '1970/02/07', 'F', 80.5, 1.72, default),
-(default, 'Jane', '2000/01/05', 'F', 60.5, 1.72, 'Brasileiro')
 
---
+++ dml.sql
@@ -0,0 +1,12 @@
+/* DML */
+/* Data Manipulation Language */
+
+insert into pessoas 
+(nome, idade, sexo, peso, altura, nacionalidade) 
+values 
+('Chico', '1970/02/07', 'M', 55.5, 1.72, 'EUA'),
+('Maria', '2000/01/05', 'M', 34.5, 1.72, 'Brasileiro');
+
+insert into pessoas values 
+(default, 'Ludiana', '1970/02/07', 'F', 80.5, 1.72, default),
+(default, 'Jane', '2000/01/05', 'F', 60.5, 1.72, 'Brasileiro')
